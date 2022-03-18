61d18f2a2f2dd7e043c1500de58b2f02594c67b4
cychung_Dadi <sherrynoneko@gmail.com>
2022-03-18 21:49:17 +0800

    2022/3/18：crosstab新增missing功能、解決總百分比不正確問題

--- Crosstab.py
+++ Crosstab.py
@@ -9,12 +9,12 @@ Created on Thu Mar 17 17:10:55 2022
 
 import pandas as pd
 
-def crosstab(dataframe,indexName,columnName,weight=None):
+def crosstab(dataframe,indexName,columnName,weight=None,colmissing=[]):
     
-    dfn = dataframe.reset_index(drop=True)
+    dfn = dataframe.loc[dataframe[columnName].isin(colmissing)==False].reset_index(drop=True).fillna(0)
 
     if weight != None:
-        dfc = pd.crosstab(dfn[indexName],dfn[columnName],dfn[weight],aggfunc = sum).reset_index()
+        dfc = pd.crosstab(dfn[indexName],dfn[columnName],dfn[weight],aggfunc = sum).reset_index().fillna(0)
     
 
     else:
@@ -24,21 +24,28 @@ def crosstab(dataframe,indexName,columnName,weight=None):
 
     v = dfc.last_valid_index()
     
-    dfc.loc[v+1,indexName] = 'Total'
+    dfc.loc[v+1,indexName] = indexName + '_Total'
     
     yitems = list(dfc.columns.values)
     yitems.remove(indexName)
-       
+    
     for i in yitems:
         
         dfc.loc[v+1,i] = dfc.loc[:v,i].sum()
-        
-        dfc[str(int(i))+'%'] = dfc[i] / dfc[yitems].sum(axis=1)   #row_percentage
+
+    dfc['Ntemp'] = dfc[yitems].sum(axis=1)    
+
+    for i in yitems:
+               
+        dfc[str(int(i))+'%'] = dfc[i] / dfc['Ntemp']   #row_percentage
         
         #dfc[str(int(i))+'%'] = dfc[i] / dfc.loc[v+1,i]  #column_percentage
     
     dfc['N'] = dfc[yitems].sum(axis=1)
-      
+    
+    dfc.drop('Ntemp',axis=1,inplace=True)
+    
+    dfc.rename(index=str,columns={indexName:'level'},inplace=True)
     
     return dfc
 
