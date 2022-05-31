45e7f5a0f6e623757ca2737fadc3abc56eed68e1
cychung_Dadi <sherrynoneko@gmail.com>
2022-05-31 16:34:30 +0800

    2022/5/13：新增大交叉表語法

--- Crosstab.py
+++ Crosstab.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 def crosstab(dataframe,indexName,columnName,weight=None,colmissing=[]):
     
-    dfn = dataframe.loc[dataframe[columnName].isin(colmissing)==False].reset_index(drop=True).fillna(0)
+    dfn = dataframe.loc[dataframe[columnName].isin(colmissing)==False].reset_index(drop=True).fillna(99)
 
     if weight != None:
         dfc = pd.crosstab(dfn[indexName],dfn[columnName],dfn[weight],aggfunc = sum).reset_index().fillna(0)
@@ -49,3 +49,34 @@ def crosstab(dataframe,indexName,columnName,weight=None,colmissing=[]):
     
     return dfc
 
+
+def main_crosstab(dataframe,columnName,weight=None,party_type=1,optional_variables=[]):
+    
+    if party_type == 1: partyr = 'supporting_partyr'
+    elif party_type == 2: partyr = 'supporting_partyrr'
+    
+    main_variables = ['gender','agegp','edugp','edugpr','q1a','supporting_party',partyr]
+
+    variables = main_variables + optional_variables
+    
+    crosstables = []
+    
+    j = 1
+    
+    for i in variables:
+    
+        locals()['C'+i] = crosstab(dataframe,i,columnName,weight)
+        
+        crosstables.append('C'+i)
+    
+    main_crosstabale = eval(crosstables[0])
+    
+    while j < len(crosstables):
+        
+        main_crosstabale = main_crosstabale.append(eval(crosstables[j]))
+        j += 1
+        
+    return main_crosstabale
+        
+        
+        
